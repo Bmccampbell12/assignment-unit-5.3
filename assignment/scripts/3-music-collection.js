7c1b3ca62e1f872d306c539ba6e294f64d37e368
Bruce McCampbell <mccampbellbruce1@gmail.com>
2024-09-03 07:10:13 -0500

    5.3 music collection.js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,6 +1,62 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = [];
+
+function addToCollection(collection, title, artist, yearPublished) {
+  const newMusic = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+
+   };
+
+  collection.push(newMusic);
+
+  return newMusic;
+};
+
+addToCollection(myCollection, 'First Class', 'Rainbow Kitten Surprise', 2024);
+addToCollection(myCollection, 'Jungle', 'Tash Sultana', 2022);
+addToCollection(myCollection, 'Be Yourself', 'Wilder Woods', 2020);
+addToCollection(myCollection, 'Too sweet', 'Hozier', 2023);
+addToCollection(myCollection, 'Devil like me', 'Rainow Kitten Surprise', 2024);
+addToCollection(myCollection, 'Dragonfly', 'Dana and Alden', 2023);
+
+console.log(myCollection);
+
+showCollection(myCollection);
+
+
+
+function showCollection(collection) {
+
+  for (let i = 0; i < collection.length; i++) {
+
+    const album = collection[i];
+
+    console.log(`${album.title} by ${album.artist} plublished in ${album.yearPublished}`) 
+   }
+}
+
+function findByArtist(collection, artist) {
+
+  const matchingAlbums = [];
+
+  for (let i = 0; i < collection.length; i++){
+
+    const album = collection[i];
+
+    if (album.artist === artist);
+
+    matchingAlbums.push(album);
+  
+  return matchingAlbums;
+
+  }
+}
+
+
 
 
 
